5f409fb4aafa9297045fd6f403fc94957670ab05
yaphone <zhouyaphone@163.com>
2016-03-02 14:49:10 +0800

    s

--- sdwn_gui/SDWN_GUI.py
+++ sdwn_gui/SDWN_GUI.py
@@ -1,6 +1,9 @@
 #coding=utf-8
-from Tkinter import *
 import os
+import tkMessageBox
+from ttk import *
+from Tkinter import *
+from PIL import Image, ImageTk
 
 class SDWN_GUI():
     def __init__(self):
@@ -10,20 +13,47 @@ class SDWN_GUI():
     def creat_widgets(self):
         
         #主窗口
-        root = Tk()
-        root.title(u"基于软件定义网络的无线局域网管理系统")
+        self.root = Tk()
+        self.root.title(u"基于软件定义网络的无线局域网管理系统 - 重庆邮电大学")
         
-        top_frame = Frame(root)
-        top_frame.grid()
+        #添加主要控件
+        self.top_frame = Frame(self.root)
+        self.menu_bar = Menu(self.root)
         
-        photo=PhotoImage(file=self.path + '/1.gif')
-        wifi_button = Label(top_frame, text="WIFI", image=photo)
         
-        wifi_button.grid()
+        self.top_frame.grid()
         
+        
+        #为顶部menu_bar添加选项
+        self.about_menu = Menu(self.menu_bar, tearoff=0)
+        self.about_menu.add_command(label=u"关于", command=self.about_info)
+        
+        self.menu_bar.add_cascade(label=u"关于", menu=self.about_menu)
+        
+        self.root.config(menu=self.menu_bar)
+        
+        #top_frame增加按键
+        self.user_button_img = Image.open(self.path + '/img/user.ico')
+        self.user_button_photo=ImageTk.PhotoImage(self.user_button_img)
+        self.user_button = Button(self.top_frame, text=u"用户管理", image=self.user_button_photo, compound=TOP)
+        
+        self.ap_button_img = Image.open(self.path + '/img/ap.ico')
+        self.ap_button_photo = ImageTk.PhotoImage(self.ap_button_img)
+        self.ap_button = Button(self.top_frame, text=u"AP管理", image=self.ap_button_photo, compound=TOP)
+        
+        
+        
+        
+        self.user_button.grid(row=0, column=0, padx=5)
+        self.ap_button.grid(row=0, column=1, padx=5)
+
         
         #程序进入主循环
-        root.mainloop()
+        self.root.mainloop()
+        
+    def about_info(self):
+        aboutStr = "基于软件定义网络的无线局域网管理系统 - 重庆邮电大学\n V1.0"
+        tkMessageBox.showinfo("关于", aboutStr)    
         
         
 if __name__ == "__main__":
